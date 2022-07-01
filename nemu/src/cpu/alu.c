57fc0903c2fc6052d8a419aab63add929d16900f
crhcrhcrhcrh <1368263361@qq.com>
2022-07-01 13:49:05 +0800

    delete

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -17,10 +17,7 @@ uint32_t alu_adc(uint32_t src, uint32_t dest, size_t data_size)
 #ifdef NEMU_REF_ALU
 	return __ref_alu_adc(src, dest, data_size);
 #else
-	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
-	fflush(stdout);
-	assert(0);
-	return 0;
+
 #endif
 }
 
