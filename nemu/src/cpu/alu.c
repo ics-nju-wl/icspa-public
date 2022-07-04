5f5e3b87c3a2ae2107a88ac18328cc55d4d41d94
tracer-pa-public <tracer@njuics.org>
2022-07-04 22:54:25 +0800

    > compile nemu testcase kernel > id icspa_mooc_student > user root > uname Linux iZwz9fv6t02o890txs91suZ 3.10.0-1160.66.1.el7.x86_64 #1 SMP Wed May 18 16:02:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux > uptime 22:54:25 up 3 days, 21:58,  0 users,  load average: 0.00, 0.02, 0.05 > unitime > bcbb0d263ef2a3fdd6d8a470c1e6f7a473319afa

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -17,7 +17,8 @@ uint32_t alu_adc(uint32_t src, uint32_t dest, size_t data_size)
 #ifdef NEMU_REF_ALU
 	return __ref_alu_adc(src, dest, data_size);
 #else
-
+	uint32_t res = 0;
+	res = dest + src;
 #endif
 }
 
