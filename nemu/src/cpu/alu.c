c9894717f11a65af8bac97240b7a7da11ae7fe38
crhcrhcrhcrh <1368263361@qq.com>
2022-07-05 16:01:37 +0800

    test PF

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -1,11 +1,32 @@
 #include "cpu/cpu.h"
-
+#define INF 0xffffffff
+#define set(bit) (INF >> (32 - bit))
 void set_CF_add(uint32_t result, uint32_t src, size_t data_size)
 {
 	result = sign_ext(result & (0xffffffff >> (32 - data_size)), data_size);
 	src = sign_ext(src & (0xffffffff >> (32 - data_size)), data_size);
 	cpu.eflags.CF = result < src;
 	
+}
+void set_ZF(uint32_t result, size_t data_size)
+{
+	result = result & (INF >> (32 - data_size));
+	cpu.eflags.ZF = (result == 0);
+}
+void set_PF(uint32_t result)
+{
+	uint8_t low8 = result & set(8);
+	int cnt = 0 ;
+	for(int i = 0; i < 8; i++)
+	{
+		if(low8 >> i & 1)cnt++;
+	}
+	cpu.eflags.PF = (cnt % 2 == 0);
+}
+void set_SF(uint32_t result, size_t data_size)
+{
+	result = sign_ext(result & set(data_size), data_size);
+	cpu.eflags.SF = sign(result);
 }
 uint32_t alu_add(uint32_t src, uint32_t dest, size_t data_size)
 {
@@ -15,7 +36,7 @@ uint32_t alu_add(uint32_t src, uint32_t dest, size_t data_size)
 	uint32_t res = dest + src;
 	set_CF_add(res, src, data_size);
 	// set_PF(res);
-	// set_ZF(res, data_size);
+	set_ZF(res, data_size);
 	// set_SF(res, data_size);
 	// set_OF_add(res, src, dest, data_size);
 	return (res) & (0xffffffff >> (32 - data_size));
